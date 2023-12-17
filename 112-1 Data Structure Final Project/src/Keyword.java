5ecba88e094b162b91390ebac680b72850ceb27a
chienstarry <111306031@nccu.edu.tw>
2023-12-17 17:43:19 +0800

    123

---
+++ 112-1 Data Structure Final Project/src/Keyword.java
@@ -0,0 +1,11 @@
+public class Keyword {
+    int a = 5;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
--- 112-1 Data Structure Final Project/src/Main.java
+++ 112-1 Data Structure Final Project/src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
-    int a = 5;
-
-    public int getA() {
-        return a;
-    }
-
-    public void setA(int a) {
-        this.a = a;
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        int a = 5;
+        System.out.println(a);
     }
 }
