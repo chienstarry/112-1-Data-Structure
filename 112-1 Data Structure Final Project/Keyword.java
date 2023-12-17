6f733ad36f30acc98b6ac3b25bf19ab08f5ff4fe
chienstarry <111306031@nccu.edu.tw>
2023-12-17 16:17:33 +0800

    test

---
+++ 112-1 Data Structure Final Project/Keyword.java
@@ -0,0 +1,11 @@
+public class Keyword {
+    int i = 5;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
--- 112-1 Data Structure Final Project/Main.java
+++ 112-1 Data Structure Final Project/Main.java
@@ -1,10 +1,14 @@
 
 public class Main {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Test
 		int i = 1;
+		System.out.println(i);
 	}
 
 }
