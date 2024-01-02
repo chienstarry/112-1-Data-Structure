bbba755e339444e85d008c31b0d4d9c7abb9469a
1deO <117028406+1deO@users.noreply.github.com>
2024-01-02 18:46:34 +0800

    update

--- testWeb/src/main/java/TestProject.java
+++ testWeb/src/main/java/TestProject.java
@@ -1,7 +1,7 @@
-
-
+import java.awt.print.Pageable;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -18,52 +18,89 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestProject")
 public class TestProject extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestProject() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TestProject() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
-		if(request.getParameter("keyword")== null) {
+		if (request.getParameter("keyword") == null) {
 			String requestUri = request.getRequestURI();
 			request.setAttribute("requestUri", requestUri);
 			request.getRequestDispatcher("Search.jsp").forward(request, response);
 			return;
 		}
-		GoogleQuery google = new GoogleQuery(request.getParameter("keyword"));
-		HashMap<String, String> query = google.query();
-		
-		String[][] s = new String[query.size()][2];
-		request.setAttribute("query", s);
-		int num = 0;
-		for(Entry<String, String> entry : query.entrySet()) {
-		    String key = entry.getKey();
-		    String value = entry.getValue();
-		    s[num][0] = key;
-		    s[num][1] = value;
-		    num++;
+		String googleInput = request.getParameter("keyword");
+        String searchKeyword = googleInput + (isEnglish(googleInput) ? " novel" : " 小說");
+		GoogleQuery google = new GoogleQuery(searchKeyword);
+		google.query();
+		ArrayList<String> googleNameList = google.outputName;
+		ArrayList<String> googleSiteList = google.outputSite;
+		ArrayList<WebPage> webPages = new ArrayList<>();
+
+		// 使用主搜索關鍵詞進行Google搜索
+		ArrayList<Keyword> keywords = KeywordConfig.getKeywords(googleInput, 1000.0);
+
+		for (int i = 0; i < googleSiteList.size(); i++) {
+			WebPage page = new WebPage(googleSiteList.get(i), googleNameList.get(i));
+			// page.setScore(keywords);
+			// webPages.add(page);
+			try {
+				page.setScore(keywords);
+				webPages.add(page);
+			} catch (IOException e) {
+				System.out.println("Error setting score for URL: " + page.url);
+				// 在这里添加逻辑以处理无法访问的网址，比如记录日志、跳过当前网址等
+				continue; // 跳过当前的循环迭代
+			}
+			System.out.println(i);
 		}
-		request.getRequestDispatcher("googleitem.jsp")
-		 .forward(request, response); 
+
+		Sort sort = new Sort(webPages);
+		sort.sortSite(0, webPages.size() - 1);
+		sort.delcontact(webPages);
 		
+		
+		
+		
+		
+		String[][] s = new String[webPages.size()][2];
+		
+		int num = 0;
+		for (WebPage page : webPages) {
+			s[num][0] = page.name;
+			s[num][1] = page.url;
+			num++;
+		}
+		request.setAttribute("query", s);
+		request.getRequestDispatcher("googleitem.jsp").forward(request, response);
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	private static boolean isEnglish(String text) {
+        return text.matches("[A-Za-z ]+");
+}
 
 }
--- testWeb/src/main/java/WebTree.java
+++ testWeb/src/main/java/WebTree.java
@@ -1,41 +1,31 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class WebTree
-{
+public class WebTree {
 	public WebNode root;
 
-	public WebTree(WebPage rootPage)
-	{
+	public WebTree(WebPage rootPage) {
 		this.root = new WebNode(rootPage);
 	}
 
-	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException
-	{
+	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException {
 		setPostOrderScore(root, keywords);
 	}
 
-	private void setPostOrderScore(WebNode startNode, ArrayList<Keyword> keywords) 
-	{
-		// YOUR TURN
-		// 3. compute the score of children nodes via post-order, then setNodeScore for
-		// startNode
+	private void setPostOrderScore(WebNode startNode, ArrayList<Keyword> keywords) {
+
 		try {
 			startNode.setNodeScore(keywords);
-//			System.out.println(startNode.webPage.name + "," + startNode.nodeScore);
-		}
-		catch(Exception e) {
-//			System.out.println("Error from: "+startNode.webPage.name);
+		} catch (Exception e) {
+
 		}
 	}
 
-	public void eularPrintTree()
-	{
+	public void eularPrintTree() {
 		eularPrintTree(root);
 	}
 
-	private void eularPrintTree(WebNode startNode)
-	{
+	private void eularPrintTree(WebNode startNode) {
 		int nodeDepth = startNode.getDepth();
 
 		if (nodeDepth > 1) {
@@ -43,10 +33,9 @@ public class WebTree
 		}
 		System.out.print("(");
 		System.out.print(startNode.webPage.name + "," + startNode.nodeScore);
-		
-		// YOUR TURN
-		// 4. print child via pre-order
-		for(int i=0;i<startNode.children.size();i++) {
+
+
+		for (int i = 0; i < startNode.children.size(); i++) {
 			eularPrintTree(startNode.children.get(i));
 		}
 		System.out.print(")");
@@ -56,11 +45,9 @@ public class WebTree
 		}
 	}
 
-	private String repeat(String str, int repeat)
-	{
+	private String repeat(String str, int repeat) {
 		String retVal = "";
-		for (int i = 0; i < repeat; i++)
-		{
+		for (int i = 0; i < repeat; i++) {
 			retVal += str;
 		}
 		return retVal;
